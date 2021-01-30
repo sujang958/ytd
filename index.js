df226c79c234f5094ee5fa94272bea9c9a82789a
sujang958 <enter7377@naver.com>
2021-01-30 14:05:15 +0900

    modifie music save  name

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ app.get('/process', (req, res) => {
         title = replaceSpecialChars(title);                                                 // 영상 제목 특수문자 제거
         var audioFiles = fs.readdirSync(__dirname + '/static/audio/');                      // 오디오 저장하는 디렉토리 스캔
 
-        if (audioFiles.includes(`${title}`)) {                                              // 오디오를 저장하는 폴더에 같은 영상이 있으면
-            res.send(sendDownloadRedirect(`/audio/${title}.wav`, `${title}.wav`));          // 기존에 있던 파일로 다운로드
+        if (audioFiles.includes(`${youtube_url}`)) {                                              // 오디오를 저장하는 폴더에 같은 영상이 있으면
+            res.send(sendDownloadRedirect(`/audio/${youtube_url}.wav`, `${title}.wav`));          // 기존에 있던 파일로 다운로드
         } else {                                                                            // 아니면
             ytdl(`https://www.youtube.com/watch?v=${youtube_url}`, { filter: 'audioonly'})  // 파일 새로 다운받고 새로운 파일로 다운로드
-            .pipe(fs.createWriteStream(`${__dirname}/static/audio/${title}.wav`));
-            res.send(sendDownloadRedirect(`/audio/${title}.wav`, `${title}.wav`));
+            .pipe(fs.createWriteStream(`${__dirname}/static/audio/${youtube_url}.wav`));
+            res.send(sendDownloadRedirect(`/audio/${youtube_url}.wav`, `${title}.wav`));
         }
     })
 })
